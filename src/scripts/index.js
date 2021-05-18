3228d89152b5fe5f1b0d3f6ed1368714ea18022e
A_Prabowo <agungprabowo8800@gmail.com>
2021-05-18 14:00:14 +0700

    load content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,43 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
-console.log('Hello Coders! :)');
+import('../DATA.json').then(({
+    default: jsonData
+}) => {
+    let dataRestos = jsonData['restaurants']
+    let listResto = ''
+    dataRestos.forEach(function(resto){
+        listResto += `
+        <div class="list_item">
+        <img class="list_thumbnail" src="${resto['pictureId']}" alt="${resto['name']}" title="${resto['name']}">
+        <div class="ratings">Rating : ${resto['rating']} / 5</div>
+        <div class="list_content">
+            <h1 class="list_item_title"><a href="#">${resto['name']} - ${resto['city']}</a></h1>
+            <div class="list_item_desc">Description : ${resto['description'].slice(0, 111)}...</div>
+        </div>
+    </div>
+        `;
+    });
+    document.querySelector('#RestaurantCatalog').innerHTML = listResto;
+})
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(event) {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
+});
+
+hero.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
+
+//console.log('Hello Coders! :)');
