ce45ddf4248c36d1821e27eaf4b7d4dabc18ef1b
A_Prabowo <agungprabowo8800@gmail.com>
2021-07-20 16:30:32 +0700

    final submission

--- e2e/Unliking_Resto.spec.js
+++ e2e/Unliking_Resto.spec.js
@@ -13,6 +13,7 @@ Before(({ I }) => {
 
 Scenario('unliking one resto', async ({ I }) => {
   I.seeElement('#restaurant-catalog .list_item');
+  I.wait(1);
   I.click('#restaurant-catalog .list_item_title a');
 
   I.seeElement('[aria-label="unlike this restaurant"]');
